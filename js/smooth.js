cf9eaf19c9487310560a4627aaa47e13a6ce265d
JuniorAlbuquerque <junioralbuquerque81@gmail.com>
2020-07-14 11:31:27 -0400

    refactor: smooth scroll function

--- js/smooth.js
+++ js/smooth.js
@@ -1,7 +1,48 @@
-class SmoothControl {
-  constructor() {}
+class SmoothScroll {
+  constructor(links) {
+    this.linkElements = document.querySelectorAll(links)
+    this.addClickEvent()
+  }
 
-  getHref() {}
+  handleClick(event) {
+    event.preventDefault()
+    const href = event.currentTarget.getAttribute('href')
+    const section = document.querySelector(href)
+    window.scrollTo({
+      top: section.offsetTop,
+      behavior: 'smooth',
+    })
+  }
+
+  addClickEvent() {
+    this.linkElements.forEach((link) => {
+      link.addEventListener('click', this.handleClick)
+    })
+  }
 }
 
-const smooth = new SmoothControl()
+const scroll = new SmoothScroll("a[href^='#']")
+const btnHome = document.querySelector('.homebutton')
+btnHome.style.display = 'none'
+
+function goToHome() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  })
+}
+
+window.onscroll = function () {
+  scrollFunction()
+}
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 300 ||
+    document.documentElement.scrollTop > 300
+  ) {
+    btnHome.style.display = 'flex'
+  } else {
+    btnHome.style.display = 'none'
+  }
+}
