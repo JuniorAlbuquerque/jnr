3fe91fe617f6fd9a6907a4ebdf8e5f3c732db3b3
Júnior Albuquerque <juioralbuquerque81@gmail.com>
2022-07-22 00:50:20 -0300

    fix: page extensions

--- next.config.js
+++ next.config.js
@@ -3,8 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   eslint: {
-    dirs: ['pages'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+    dirs: ['pages'] // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
   },
+  pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js']
 }
 
 module.exports = nextConfig
