4b44f93ea079b2afed810751480fd944c0a84036
Júnior Albuquerque <juioralbuquerque81@gmail.com>
2022-07-21 22:32:50 -0300

    chore: add dependencies

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
 import { darkTheme } from '../styles/theme'
-import { globalStyles } from '../styles/global'
+import { globalStyles } from '@/styles/global'
 
 function MyApp({ Component, pageProps }: AppProps) {
   globalStyles()
